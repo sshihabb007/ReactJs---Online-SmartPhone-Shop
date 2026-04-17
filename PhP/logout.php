d484d0df4a23163837fc8032e64921ed7eb66b81
Mehedi Hasan Shihab <sshihabb007@gmail.com>
2026-04-17 08:28:03 +0600

    Cleanup: remove legacy PHP, CSS, and jQuery/JavaScript folders ensuring a clean React/Node stack

--- PhP/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('database.php');
-session_start();
-$sql="DELETE FROM `chart` WHERE `Email` = '".$_SESSION['user']."'";
-updateSQL($sql);
-unset($_COOKIE['permission']);
-session_destroy();
-header("Location:homepage");
-?>
